1393506516c76873b233e7d39d55759b6160be8c
ElvishJerricco <elvishjerricco@gmail.com>
2016-04-22 16:27:07 -0400

    Small formatting changes for readability and debuggability

--- src/Control/Monad/Trans/Fraxl.hs
+++ src/Control/Monad/Trans/Fraxl.hs
@@ -58,24 +58,25 @@ instance Monad m => DataSource (Union '[]) m where
 instance ( DataSource f m
          , DataSource (Union r) m)
          => DataSource (Union (f ': r)) m where
-  fetch list = (\(_, _, x) -> x) <$> run ANil ANil list where
-    run :: ASeq f a
+  fetch list = (\(_, _, x) -> x) <$> runUnion ANil ANil list where
+    runUnion :: ASeq f a
         -> ASeq (Union r) a'
         -> ASeq (Union (f ': r)) a''
         -> m (ASeq m a, ASeq m a', ASeq m a'')
-    run flist ulist ANil = (, , ANil) <$> fetch flist <*> fetch ulist
-    run flist ulist (ACons u us) = case prj u of
+    runUnion flist ulist ANil = (, , ANil) <$> fetch flist <*> fetch ulist
+    runUnion flist ulist (ACons u us) = case prj u of
       Right (fa :: f a) -> fmap
         (\(ACons ma ms, other, rest) -> (ms, other, ACons ma rest))
-        (run (ACons fa flist) ulist us)
+        (runUnion (ACons fa flist) ulist us)
       Left u' -> fmap
         (\(other, ACons ma ms, rest) -> (other, ms, ACons ma rest))
-        (run flist (ACons u' ulist) us)
+        (runUnion flist (ACons u' ulist) us)
 
 runFraxl :: forall f m a. DataSource f m => FreerT f m a -> m a
-runFraxl = iterT $ \a -> unAp a (\f s -> join (reduceASeq <$> fetch s) >>= f) (const id) ANil
+runFraxl = iterT $ \a -> unAp a
+  (\f s -> join (reduceASeq <$> fetch s) >>= f) (const id) ANil
 
-simpleAsyncFetch :: forall m f a. MonadIO m
+simpleAsyncFetch :: MonadIO m
                     => (forall x. f x -> IO x)
                     -> Fetch f m a
 simpleAsyncFetch fetchIO
@@ -89,21 +90,25 @@ instance ( DataSource f m
          , GCompare f
          , MonadIO (t m))
          => DataSource (CachedFetch f) (t m) where
-  fetch list = snd <$> run ANil list where
-    run :: ASeq f a
-        -> ASeq (CachedFetch f) a'
-        -> t m (ASeq (t m) a, ASeq (t m) a')
-    run flist ANil = (, ANil) <$> lift (hoistASeq lift <$> fetch flist)
-    run flist (ACons (CachedFetch f) fs) = do
+  fetch list = snd <$> runCached ANil list where
+    runCached :: ASeq f a
+              -> ASeq (CachedFetch f) a'
+              -> t m (ASeq (t m) a, ASeq (t m) a')
+    runCached flist ANil = (, ANil) <$> lift (hoistASeq lift <$> fetch flist)
+    runCached flist (ACons (CachedFetch f) fs) = do
       cache <- get
       case DMap.lookup f cache of
-        Just mvar -> fmap (second (ACons (liftIO $ readMVar mvar))) (run flist fs)
+        Just mvar -> fmap
+          (second (ACons (liftIO $ readMVar mvar)))
+          (runCached flist fs)
         Nothing -> do
           (mvar :: MVar x) <- liftIO newEmptyMVar
           put (DMap.insert f mvar cache)
           let store :: t m x -> t m x
               store m = m >>= \a -> liftIO (putMVar mvar a) >> return a
-          fmap (\(ACons m ms, rest) -> (ms, ACons (store m) rest)) (run (ACons f flist) fs)
+          fmap
+            (\(ACons m ms, rest) -> (ms, ACons (store m) rest))
+            (runCached (ACons f flist) fs)
 
 runCachedFraxl :: forall m f a.
                   ( MonadIO m
